84a27b3ed7dfbf00c2b3c6d0c3bd8b9589e5e54f
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2023-12-27 00:13:40 +0100

    add lexer tests

--- test/compiler/lexer/LexerTest.cpp
+++ test/compiler/lexer/LexerTest.cpp
@@ -41,8 +41,86 @@ void LexerTest::test ( void ) {
     }
 
     {
+        std::istringstream iss ("bigger(elephant, horse).");
+        Lexer lex ( iss );
+        assert ( lex.getToken() == TOK_ATOM_LOWER );
+        assert ( lex.identifier() == "bigger" );
+
+        assert ( lex.getToken() == TOK_LPAR );
+
+        assert ( lex.getToken() == TOK_ATOM_LOWER );
+        assert ( lex.identifier() == "elephant" );
+
+        assert ( lex.getToken() == TOK_COMMA );
+
+        assert ( lex.getToken() == TOK_ATOM_LOWER );
+        assert ( lex.identifier() == "horse" );
+
+        assert ( lex.getToken() == TOK_RPAR );
+
+        assert ( lex.getToken() == TOK_PERIOD );
+    }
+
+    {
+        std::istringstream iss ("is_bigger(X, Y) :- bigger(X, Z), is_bigger(Z, Y).");
+        Lexer lex ( iss );
+        assert ( lex.getToken() == TOK_ATOM_LOWER );
+        assert ( lex.identifier() == "is_bigger" );
+
+        assert ( lex.getToken() == TOK_LPAR );
+
+        assert ( lex.getToken() == TOK_ATOM_VAR );
+        assert ( lex.identifier() == "X" );
+
+        assert ( lex.getToken() == TOK_COMMA );
+
+        assert ( lex.getToken() == TOK_ATOM_VAR );
+        assert ( lex.identifier() == "Y" );
+
+        assert ( lex.getToken() == TOK_RPAR );
+
+        assert ( lex.getToken() == TOK_IF );
+
+        assert ( lex.getToken() == TOK_ATOM_LOWER );
+        assert ( lex.identifier() == "bigger" );
+
+        assert ( lex.getToken() == TOK_LPAR );
+
+        assert ( lex.getToken() == TOK_ATOM_VAR );
+        assert ( lex.identifier() == "X" );
+
+        assert ( lex.getToken() == TOK_COMMA );
+
+        assert ( lex.getToken() == TOK_ATOM_VAR );
+        assert ( lex.identifier() == "Z" );
+
+        assert ( lex.getToken() == TOK_RPAR );
+
+        assert ( lex.getToken() == TOK_COMMA );
+
+        assert ( lex.getToken() == TOK_ATOM_LOWER );
+        assert ( lex.identifier() == "is_bigger" );
+
+        assert ( lex.getToken() == TOK_LPAR );
+
+        assert ( lex.getToken() == TOK_ATOM_VAR );
+        assert ( lex.identifier() == "Z" );
+
+        assert ( lex.getToken() == TOK_COMMA );
+
+        assert ( lex.getToken() == TOK_ATOM_VAR );
+        assert ( lex.identifier() == "Y" );
+
+        assert ( lex.getToken() == TOK_RPAR );
+
+        assert ( lex.getToken() == TOK_PERIOD );
+    }
+
+    {
         std::istringstream iss ("  \t    \n\r\t            \n");
         Lexer lex ( iss );
         assert ( lex.getToken() == TOK_EOF );
     }
+
+    std::cout << "Lexer Test suite completed :)" << std::endl;
 }
