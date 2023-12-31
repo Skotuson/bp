146c5c1a2d149e1af0c9566ad4928f314ea2649c
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2023-12-31 15:00:02 +0100

    create Compiler class

---
+++ src/compiler/Compiler.hpp
@@ -0,0 +1,13 @@
+#ifndef COMPILER_H
+#define COMPILER_H
+
+#include <iostream>
+
+class Compiler {
+    public:
+        Compiler ( std::istream & is, 
+                   std::ostream & os );
+    private:
+};
+
+#endif
