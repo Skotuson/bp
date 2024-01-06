d336d1df793ae71de075b668f943092433c8a91e
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-01-06 19:13:01 +0100

    start compiler module implementation

--- src/compiler/Compiler.cpp
+++ src/compiler/Compiler.cpp
@@ -1,1 +1,12 @@
 #include "Compiler.hpp"
+
+#include "ast/AST.hpp"
+
+Compiler::Compiler ( std::istream & is, std::ostream & os )
+: m_Parser ( Lexer ( is ) )
+{}
+
+void Compiler::compile ( void ) {
+    m_Parser . parse ( );
+    ProgramNode * astRoot = m_Parser . getAST();
+}
--- src/compiler/Compiler.hpp
+++ src/compiler/Compiler.hpp
@@ -2,12 +2,17 @@
 #define COMPILER_H
 
 #include <iostream>
+#include "lexer/Lexer.hpp"
+#include "parser/Parser.hpp"
 
 class Compiler {
     public:
         Compiler ( std::istream & is, 
                    std::ostream & os );
+
+        void compile ( void );
     private:
+        Parser m_Parser = Parser( Lexer ( std::cin ) );
 };
 
 #endif
