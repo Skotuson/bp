a4dbae491bdbd795cae8a70681ff579beced0313
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-01-28 00:25:24 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,26 @@
 #include <cassert>
 #include <iostream>
+#include <fstream>
 
-#include "compiler/lexer/Lexer.hpp"
+#include "compiler/Compiler.hpp"
 
-int main ( void ) {
+int main ( int argc, const char ** argv ) {
+    std::ostream & os = std::cout;    
+
+    if ( argc != 2 ) {
+        std::cout << "Usage: [filename]" << std::endl;
+        return 0;
+    }
+
+    std::ifstream ifs ( argv[1] );
+
+    if ( ! ifs ) {
+        std::cout << "File couldn't be opened" << std::endl;
+        return 0;
+    } 
+
+    Compiler comp(ifs, os);
+    comp.compile();
+    os << std::endl;
     return 0;
 }
