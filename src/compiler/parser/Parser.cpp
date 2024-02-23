39c1c3a72a1162f9d52f27d67fff29b08a4c31ef
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-02-23 15:31:03 +0100

    add number parsing

--- src/compiler/parser/Parser.cpp
+++ src/compiler/parser/Parser.cpp
@@ -194,9 +194,9 @@ TermNode *Parser::Term(void)
     case TOK_ATOM_LOWER:
         m_Lex.match(TOK_ATOM_LOWER);
         return TermLower();
-    // case TOK_CONST:
-    //     m_Lex . match ( TOK_CONST );
-    //     return new ConstNode( );
+    case TOK_CONST:
+        m_Lex . match ( TOK_CONST );
+        return new ConstNode( m_Lex.numericValue() );
     case TOK_LSPAR:
         m_Lex.match(TOK_LSPAR);
         list = ListInner();
