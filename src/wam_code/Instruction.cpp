5cdbf441a597c4bf482154cd4ec638892470fdb3
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-02-23 23:47:46 +0100

    add more instructions, expand implementation

--- src/wam_code/Instruction.cpp
+++ src/wam_code/Instruction.cpp
@@ -6,9 +6,14 @@ void MarkInstruction::print(std::ostream &os)
     os << "mark";
 }
 
+RetryMeElseInstruction::RetryMeElseInstruction(const std::string &label)
+    : m_Label(label)
+{
+}
+
 void RetryMeElseInstruction::print(std::ostream &os)
 {
-    os << "retry-me-else";
+    os << "retry-me-else" << m_Label;
 }
 
 // Procedural Instructions
@@ -17,13 +22,63 @@ void CallInstruction::print(std::ostream &os)
     os << "call";
 }
 
+void ReturnInstruction::print(std::ostream &os)
+{
+    os << "return";
+}
+
 // Get Instructions
+
+GetInstruction::GetInstruction(const std::string &name, size_t argumentRegister)
+    : m_Name(name),
+      m_ArgumentRegister(argumentRegister)
+{
+}
+
+GetConstantInstruction::GetConstantInstruction(const std::string &name, size_t argumentRegister)
+    : GetInstruction(name, argumentRegister)
+{
+}
+
 void GetConstantInstruction::print(std::ostream &os)
 {
-    os << "get-constant";
+    os << "get-constant " << m_Name;
+}
+
+GetVariableInstruction::GetVariableInstruction(const std::string &name, size_t argumentRegister)
+    : GetInstruction(name, argumentRegister)
+{
 }
 
 void GetVariableInstruction::print(std::ostream &os)
 {
-    os << "getv";
+    os << "getv" << m_Name;
+}
+
+// Put Instructions
+
+PutInstruction::PutInstruction(const std::string &name, size_t argumentRegister)
+    : m_Name(name),
+      m_ArgumentRegister(argumentRegister)
+{
+}
+
+PutConstantInstruction::PutConstantInstruction(const std::string &name, size_t argumentRegister)
+    : PutInstruction(name, argumentRegister)
+{
+}
+
+void PutConstantInstruction::print(std::ostream &os)
+{
+    os << "put-constant " << m_Name;
+}
+
+PutVariableInstruction::PutVariableInstruction(const std::string &name, size_t argumentRegister)
+    : PutInstruction(name, argumentRegister)
+{
+}
+
+void PutVariableInstruction::print(std::ostream &os)
+{
+    os << "putv" << m_Name;
 }
--- src/wam_code/Instruction.hpp
+++ src/wam_code/Instruction.hpp
@@ -2,11 +2,12 @@
 #define INSTRUCTION_H
 
 #include <ostream>
+#include <string>
 
 struct Instruction
 {
     // virtual void execute(void) = 0;
-    virtual void print(std::ostream & os) = 0;
+    virtual void print(std::ostream &os) = 0;
 };
 
 // Indexing instructions
@@ -17,7 +18,10 @@ struct MarkInstruction : public Instruction
 
 struct RetryMeElseInstruction : public Instruction
 {
+    RetryMeElseInstruction(const std::string &label);
     void print(std::ostream &os) override;
+
+    std::string m_Label;
 };
 
 struct BacktrackInstruction : public Instruction
@@ -32,27 +36,69 @@ struct CallInstruction : public Instruction
 
 struct ReturnInstruction : public Instruction
 {
+    void print(std::ostream &os) override;
 };
 
 // Get Instructions
-struct GetConstantInstruction : public Instruction
+struct GetInstruction : public Instruction
 {
+protected:
+    GetInstruction(const std::string &name, size_t argumentRegister);
+    std::string m_Name;
+    size_t m_ArgumentRegister;
+};
+
+struct GetConstantInstruction : public GetInstruction
+{
+    GetConstantInstruction(const std::string &name, size_t argumentRegister);
     void print(std::ostream &os) override;
 };
 
-struct GetListInstruction : public Instruction
+struct GetListInstruction : public GetInstruction
 {
 };
 
-struct GetStructureInstruction : public Instruction
+struct GetStructureInstruction : public GetInstruction
 {
 };
 
-struct GetVariableInstruction : public Instruction
+struct GetVariableInstruction : public GetInstruction
 {
+    GetVariableInstruction(const std::string &name, size_t argumentRegister);
     void print(std::ostream &os) override;
 };
 // Put Instructions
+struct PutInstruction : public Instruction
+{
+protected:
+    PutInstruction(const std::string &name, size_t argumentRegister);
+    std::string m_Name;
+    size_t m_ArgumentRegister;
+};
+struct PutConstantInstruction : public PutInstruction
+{
+    PutConstantInstruction(const std::string &name, size_t argumentRegister);
+    void print(std::ostream &os) override;
+};
+
+struct PutVariableInstruction : public PutInstruction
+{
+    PutVariableInstruction(const std::string &name, size_t argumentRegister);
+    void print(std::ostream &os) override;
+};
+
+struct PutStructureInstruction : public PutInstruction
+{
+};
+
 // Unify Instructions
+
+struct UnifyConstantInstruction : public Instruction
+{
+};
+
+struct UnifyVariableInstruction : public Instruction
+{
+};
 
 #endif // INSTRUCTION_H
