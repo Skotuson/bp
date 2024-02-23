20023baabb797e6a52037420d75a07805db7ffee
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-02-23 16:26:03 +0100

    rename SymbolTable -> CompilationContext

--- src/compiler/CompilationContext.hpp
+++ src/compiler/CompilationContext.hpp
@@ -1,8 +1,11 @@
 #ifndef SYMBOLTABLE_H
 #define SYMBOLTABLE_H
 
+#include <map>
 #include <string>
 #include <unordered_map>
+
+using Label = std::string;
 
 struct TableEntry
 {
@@ -16,7 +19,7 @@ struct TableEntry
     size_t m_Clauses = 1;
 };
 
-class SymbolTable
+class CompilationContext
 {
 public:
     void add(const std::string &symbol,
@@ -24,7 +27,9 @@ public:
     TableEntry *get(const std::string &symbol);
 
 private:
-    std::unordered_map<std::string, TableEntry *> m_Table;
+    std::unordered_map<std::string, TableEntry *> m_SymbolTable;
+    size_t m_Line = 0;
+    std::map<Label, size_t> labels;
 };
 
 #endif
