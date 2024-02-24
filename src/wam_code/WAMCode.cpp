ceabceb4ca632dc0569b5fda6995323d2400d0eb
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-02-24 18:05:27 +0100

    add dump function to WAMCode

--- src/wam_code/WAMCode.cpp
+++ src/wam_code/WAMCode.cpp
@@ -1,5 +1,15 @@
 #include "WAMCode.hpp"
 
-void WAMCode::addInstructions(std::vector<Instruction> & instructions) {
+void WAMCode::addInstructions(std::vector<Instruction*> &instructions)
+{
     program.insert(program.end(), instructions.begin(), instructions.end());
+}
+
+void WAMCode::dump(std::ostream &os)
+{
+    for (const auto &instruction : program)
+    {
+        instruction->print(os);
+        os << std::endl;
+    }
 }
--- src/wam_code/WAMCode.hpp
+++ src/wam_code/WAMCode.hpp
@@ -6,15 +6,16 @@
 #include <map>
 #include <vector>
 #include <string>
+#include <ostream>
 
 using Label = std::string;
 
 struct WAMCode
 {
-    void addInstructions(std::vector<Instruction> & instructions);
-
+    void addInstructions(std::vector<Instruction*> & instructions);
+    void dump(std::ostream & os);
     std::map<Label, size_t> labels;
-    std::vector<Instruction> program;
+    std::vector<Instruction*> program;
 };
 
 #endif // WAMCODE_H
