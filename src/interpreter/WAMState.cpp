6983256e917e471e7d298d653e8abe374dbafb5e
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-02-26 12:44:19 +0100

    add fillRegister method for working with arg regs

--- src/interpreter/WAMState.cpp
+++ src/interpreter/WAMState.cpp
@@ -1,1 +1,8 @@
 #include "WAMState.hpp"
+
+void WAMState::fillRegister(Word *word, size_t reg)
+{
+    if(m_ArgumentRegisters.capacity() < reg)
+        m_ArgumentRegisters.reserve(reg*2);
+    m_ArgumentRegisters[reg - 1] = word;
+}
--- src/interpreter/WAMState.hpp
+++ src/interpreter/WAMState.hpp
@@ -1,14 +1,19 @@
 #ifndef WAMSTATE_H
 #define WAMSTATE_H
 
+#include "data_structures/Word.hpp"
 #include "data_structures/ChoicePoint.hpp"
 
 #include <stack>
+#include <vector>
 #include <cstdlib>
 
 struct WAMState
 {
+    void fillRegister(Word *word, size_t reg);
+
     std::stack<ChoicePoint> m_Stack;
+    std::vector<Word *> m_ArgumentRegisters;
     size_t m_ProgramCounter = 0;
 };
 
