e032d3cd5c60e6dc42d708adae65060f441275bb
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-02-26 22:22:09 +0100

    add program counter and mode flag to wamstate

--- src/interpreter/WAMState.hpp
+++ src/interpreter/WAMState.hpp
@@ -13,9 +13,13 @@ struct WAMState
 {
     void fillRegister(Word *word, size_t reg);
 
+    size_t m_ProgramCounter = 0;
+    size_t m_ContinuationPointer = 0;
+
     std::stack<ChoicePoint> m_Stack;
     ArgumentRegisters m_ArgumentRegisters;
-    size_t m_ProgramCounter = 0;
+
+    bool m_ModeFlag = false;
 };
 
 #endif // WAMSTATE_H
