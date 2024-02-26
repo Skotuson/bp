dd07afca25f6bbc1d20a65959d476e37144cd658
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-02-26 22:21:41 +0100

    add choice point variables

--- src/interpreter/data_structures/ChoicePoint.hpp
+++ src/interpreter/data_structures/ChoicePoint.hpp
@@ -1,8 +1,15 @@
 #ifndef CHOICEPOINT_H
 #define CHOICEPOINT_H
 
+#include "ArgumentRegisters.hpp"
+
 struct ChoicePoint
 {
+    ArgumentRegisters m_ArgumentRegisters;
+    size_t m_BCP; // Backtrack Continuation Pointer
+    size_t m_BCE; // Backtrack Continuation Environment
+    size_t m_BB;  // Backtrack B (register)
+    size_t m_FA;  // Failure Address
 };
 
 #endif // CHOICEPOINT_H
