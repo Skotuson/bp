5ee0d3f640e70f2bb73ef5a143d6359e9328ccdc
Skotuson <58478268+Skotuson@users.noreply.github.com>
2024-02-28 21:55:59 +0100

    add clone method

--- src/interpreter/data_structures/Word.cpp
+++ src/interpreter/data_structures/Word.cpp
@@ -1,10 +1,17 @@
 #include "Word.hpp"
 
 Word::Word(TAG tag)
-    : m_Tag(tag){};
+    : m_Tag(tag)
+{
+}
 
 ConstantWord::ConstantWord(const std::string &value)
     : Word(TAG::CONSTANT),
       m_Value(value)
 {
+}
+
+Word *ConstantWord::clone(void)
+{
+    return new ConstantWord(m_Value);
 }
--- src/interpreter/data_structures/Word.hpp
+++ src/interpreter/data_structures/Word.hpp
@@ -17,12 +17,18 @@ class Word
 {
 protected:
     Word(TAG tag);
+
+    virtual Word *clone(void) = 0;
+
     TAG m_Tag;
 };
 
 class ConstantWord : public Word
 {
     ConstantWord(const std::string &value);
+
+    Word *clone(void) override;
+    
     std::string m_Value;
 };
 
