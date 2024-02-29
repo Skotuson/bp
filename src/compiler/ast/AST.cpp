6742736affd390f51c7a7d5e4772e6b0bb2a180a
Skotuson <58478268+Skotuson@users.noreply.github.com>
2024-02-29 16:32:33 +0100

    update RetryMeElseInstruction call

--- src/compiler/ast/AST.cpp
+++ src/compiler/ast/AST.cpp
@@ -266,7 +266,7 @@ std::string ClauseNode::codegen(CompilationContext &cctx)
     ++entry->m_Generated;
     std::string retryLabel = entry->m_Generated == entry->m_Clauses ? "quit" : m_Head + std::to_string(entry->m_Generated);
     code += "\tretry-me-else " + retryLabel + "\n";
-    cctx.addInstructions({new RetryMeElseInstruction(retryLabel)});
+    cctx.addInstructions({new RetryMeElseInstruction(retryLabel, cctx.getLabelAddress(retryLabel))});
 
     size_t currentArgumentRegister = 1;
     for (size_t i = 0; i < m_Args.size(); i++)
