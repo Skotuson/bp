31fce242358c9961689702d6d40c2ea7f3481215
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-02-29 23:32:03 +0100

    add << operator for choice point

--- src/interpreter/data_structures/ChoicePoint.cpp
+++ src/interpreter/data_structures/ChoicePoint.cpp
@@ -8,3 +8,15 @@ ChoicePoint::ChoicePoint(const ArgumentRegisters &argReg, size_t bcp, size_t bce
       m_FA(fa)
 {
 }
+
+std::ostream &operator<<(std::ostream &os, const ChoicePoint &cp)
+{
+  os << "-------------------------" << std::endl;
+  os << cp.m_ArgumentRegisters;
+  os << "BCP:" << cp.m_BCP << std::endl;
+  os << "BCE:" << cp.m_BCE << std::endl;
+  os << "BB:" << cp.m_BB << std::endl;
+  os << "FA:" << cp.m_FA << std::endl;
+  os << "-------------------------";
+  return os;
+}
--- src/interpreter/data_structures/ChoicePoint.hpp
+++ src/interpreter/data_structures/ChoicePoint.hpp
@@ -7,6 +7,8 @@ struct ChoicePoint
 {
     ChoicePoint(const ArgumentRegisters &argReg, size_t bcp, size_t bce, size_t bb, size_t fa);
 
+    friend std::ostream &operator<<(std::ostream &os, const ChoicePoint &cp);
+
     ArgumentRegisters m_ArgumentRegisters;
     size_t m_BCP; // Backtrack Continuation Pointer
     size_t m_BCE; // Backtrack Continuation Environment
