1e6325a404e04adefeaddfc39a395ec932800af9
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-03-01 16:33:04 +0100

    add label to address translation methods

--- src/wam_code/WAMCode.cpp
+++ src/wam_code/WAMCode.cpp
@@ -5,6 +5,16 @@ void WAMCode::addInstructions(const std::vector<Instruction *> &instructions)
     m_Program.insert(m_Program.end(), instructions.begin(), instructions.end());
 }
 
+void WAMCode::popInstructions(size_t n)
+{
+    while (n--)
+    {
+        Instruction * instr = m_Program.back();
+        m_Program.pop_back();
+        delete instr;
+    }
+}
+
 Instruction *WAMCode::getInstruction(size_t pc)
 {
     if (pc >= m_Program.size())
@@ -34,4 +44,16 @@ void WAMCode::addLabel(const Label &label)
 {
     m_AddressToLabel.insert({m_Program.size(), label});
     m_LabelToAddress.insert({label, m_Program.size()});
+}
+
+void WAMCode::removeLabel(const Label&label)
+{
+    size_t address = m_LabelToAddress[label];
+    m_LabelToAddress.erase(label);
+    m_AddressToLabel.erase(address);
+}
+
+size_t WAMCode::getLabelAddress(const Label&label)
+{
+    return m_LabelToAddress[label];
 }
--- src/wam_code/WAMCode.hpp
+++ src/wam_code/WAMCode.hpp
@@ -13,9 +13,12 @@ using Label = std::string;
 struct WAMCode
 {
     void addInstructions(const std::vector<Instruction *> &instructions);
+    void popInstructions(size_t n);
     Instruction * getInstruction(size_t pc);
     void dump(std::ostream &os);
     void addLabel(const Label & label);
+    void removeLabel(const Label & label);
+    size_t getLabelAddress(const Label&label);
 
     std::map<size_t, Label> m_AddressToLabel;
     std::map<Label, size_t> m_LabelToAddress;
