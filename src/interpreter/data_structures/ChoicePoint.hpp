7d036c39c475af26e359aae3444037cf4eda5e88
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-03-02 16:28:47 +0100

    change argument order in choice point

--- src/interpreter/data_structures/ChoicePoint.hpp
+++ src/interpreter/data_structures/ChoicePoint.hpp
@@ -5,13 +5,13 @@
 
 struct ChoicePoint
 {
-    ChoicePoint(const ArgumentRegisters &argReg, size_t bcp, size_t bce, size_t bb, size_t fa);
+    ChoicePoint(const ArgumentRegisters &argReg, size_t bce, size_t bcp, size_t bb, size_t fa);
 
     friend std::ostream &operator<<(std::ostream &os, const ChoicePoint &cp);
 
     ArgumentRegisters m_ArgumentRegisters;
-    size_t m_BCP; // Backtrack Continuation Pointer
     size_t m_BCE; // Backtrack Continuation Environment
+    size_t m_BCP; // Backtrack Continuation Pointer
     size_t m_BB;  // Backtrack B (register)
     size_t m_FA;  // Failure Address
 };
