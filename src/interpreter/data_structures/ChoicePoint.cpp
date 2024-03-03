ea002f36d6e6ee15f559941383bfd438fbe3c437
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-03-03 17:55:47 +0100

    change initialization order

--- src/interpreter/data_structures/ChoicePoint.cpp
+++ src/interpreter/data_structures/ChoicePoint.cpp
@@ -2,8 +2,8 @@
 
 ChoicePoint::ChoicePoint(const ArgumentRegisters &argReg, size_t bcp, size_t bce, size_t bb, size_t fa)
     : m_ArgumentRegisters(argReg),
-      m_BCP(bcp),
       m_BCE(bce),
+      m_BCP(bcp),
       m_BB(bb),
       m_FA(fa)
 {
