ddc360744f95669ef20cea92922cd3b1fab4beef
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-04-13 00:34:43 +0200

    add mul and fact to example

--- examples/add.pl
+++ examples/add.pl
@@ -1,3 +1,12 @@
 add(0,Y,Y).
 add(s(0),Y,s(Y)).
 add(s(X),Y,s(Z)):- add(X,Y,Z).
+
+mul(0,X,0).
+mul(s(A),B,C) :-
+    mul(A,B,D),
+    add(D,B,C).
+
+fact(0,s(0)).
+fact(s(A),F) :- 
+    fact(A,Y), mul(s(A),Y,F).
