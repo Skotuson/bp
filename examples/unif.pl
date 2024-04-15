9b92b2dfd8e944d8873fab214f5dbc3d44f0e7e6
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-04-15 14:45:34 +0200

    Update unif.pl

--- examples/unif.pl
+++ examples/unif.pl
@@ -5,3 +5,6 @@ eats(dish(pancakes,syrup)).
 eats(dish(olives,martini)).
 jeff:-eats(X),X=olives.
 martha:-eats(X),X=49.
+alice:-eats(X),X=dish(pancakes, syrup).
+bob:-eats(X),X=Y,Y=dish(olives,martini).
+nonsensor:-dish(olives,martini)=dish(vodka,soda).
