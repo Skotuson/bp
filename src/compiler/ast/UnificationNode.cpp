65a0f287a06d9f9a00a3d5d7df80c9c6c341b920
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-04-15 14:34:27 +0200

    add unif node print and codegen

--- src/compiler/ast/UnificationNode.cpp
+++ src/compiler/ast/UnificationNode.cpp
@@ -8,16 +8,28 @@ UnificationNode::UnificationNode(TermNode *x, TermNode *y)
 
 void UnificationNode::codegen(CompilationContext &cctx)
 {
-  m_X ->m_IsGoal = true;
-  m_X ->m_IsArg = true;
-  m_Y ->m_IsGoal = true;
-  m_Y ->m_IsArg = true;
-  
-  m_X->codegen(cctx);
-  m_Y->codegen(cctx);
+  std::vector<TermNode*> args = {m_X, m_Y};
+
+  for(const auto &arg : args)
+  {
+    arg->m_IsArg = true;
+    arg->m_IsGoal = true;
+    arg->m_AvailableReg = m_AvailableReg;
+    arg->codegen(cctx);
+    m_AvailableReg = arg->m_AvailableReg;
+  }
+
   // TODO: non collision naming, add some legit name
   std::string tempVariable = "__temp" + std::to_string(cctx.allocate()) + "__";
   cctx.noteVariable(tempVariable);
   cctx.addInstruction(std::make_shared<GetVariableInstruction>(tempVariable, m_AvailableReg - 2, cctx.getVarOffset(tempVariable)));
   cctx.addInstruction(std::make_shared<GetVariableInstruction>(tempVariable, m_AvailableReg - 1, cctx.getVarOffset(tempVariable)));
+}
+
+void UnificationNode::print(const std::string &indent)
+{
+  std::cout << indent << "=======[Start UnificationNode]======" << std::endl;
+  m_X->print(indent + "");
+  m_Y->print(indent + "");
+  std::cout << indent << "=======[End UnificationNode]======" << std::endl;
 }
