fcfcdfb94415720e4dec61fd828fd8be45b49640
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-04-15 10:55:36 +0200

    update codegen for unification =

--- src/compiler/ast/UnificationNode.cpp
+++ src/compiler/ast/UnificationNode.cpp
@@ -8,5 +8,16 @@ UnificationNode::UnificationNode(TermNode *x, TermNode *y)
 
 void UnificationNode::codegen(CompilationContext &cctx)
 {
-    
+  m_X ->m_IsGoal = true;
+  m_X ->m_IsArg = true;
+  m_Y ->m_IsGoal = true;
+  m_Y ->m_IsArg = true;
+  
+  m_X->codegen(cctx);
+  m_Y->codegen(cctx);
+  // TODO: non collision naming, add some legit name
+  std::string tempVariable = "__temp" + std::to_string(cctx.allocate()) + "__";
+  cctx.noteVariable(tempVariable);
+  cctx.addInstruction(std::make_shared<GetVariableInstruction>(tempVariable, m_AvailableReg - 2, cctx.getVarOffset(tempVariable)));
+  cctx.addInstruction(std::make_shared<GetVariableInstruction>(tempVariable, m_AvailableReg - 1, cctx.getVarOffset(tempVariable)));
 }
