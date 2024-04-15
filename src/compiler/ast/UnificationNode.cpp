d662edc6061132ff203d8492a0fd2469f3b70dce
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-04-15 23:12:44 +0200

    update unification node codegen

--- src/compiler/ast/UnificationNode.cpp
+++ src/compiler/ast/UnificationNode.cpp
@@ -1,4 +1,5 @@
 #include "UnificationNode.hpp"
+#include "StructNode.hpp"
 
 UnificationNode::UnificationNode(TermNode *x, TermNode *y)
     : m_X(x),
@@ -8,22 +9,24 @@ UnificationNode::UnificationNode(TermNode *x, TermNode *y)
 
 void UnificationNode::codegen(CompilationContext &cctx)
 {
-  std::vector<TermNode*> args = {m_X, m_Y};
+  StructNode* unifStruct = new StructNode("=", {m_X, m_Y});
+  unifStruct->m_AvailableReg = m_AvailableReg;
+  unifStruct->codegen(cctx);
+  // std::vector<TermNode*> args = {m_X, m_Y};
+  // for(const auto &arg : args)
+  //{
+  //   arg->m_IsArg = true;
+  //   arg->m_IsGoal = true;
+  //   arg->m_AvailableReg = m_AvailableReg;
+  //   arg->codegen(cctx);
+  //   m_AvailableReg = arg->m_AvailableReg;
+  // }
 
-  for(const auto &arg : args)
-  {
-    arg->m_IsArg = true;
-    arg->m_IsGoal = true;
-    arg->m_AvailableReg = m_AvailableReg;
-    arg->codegen(cctx);
-    m_AvailableReg = arg->m_AvailableReg;
-  }
-
-  // TODO: non collision naming, add some legit name
-  std::string tempVariable = "__temp" + std::to_string(cctx.allocate()) + "__";
-  cctx.noteVariable(tempVariable);
-  cctx.addInstruction(std::make_shared<GetVariableInstruction>(tempVariable, m_AvailableReg - 2, cctx.getVarOffset(tempVariable)));
-  cctx.addInstruction(std::make_shared<GetVariableInstruction>(tempVariable, m_AvailableReg - 1, cctx.getVarOffset(tempVariable)));
+  //// TODO: non collision naming, add some legit name
+  // std::string tempVariable = "__temp" + std::to_string(cctx.allocate()) + "__";
+  // cctx.noteVariable(tempVariable);
+  // cctx.addInstruction(std::make_shared<GetVariableInstruction>(tempVariable, m_AvailableReg - 2, cctx.getVarOffset(tempVariable)));
+  // cctx.addInstruction(std::make_shared<GetVariableInstruction>(tempVariable, m_AvailableReg - 1, cctx.getVarOffset(tempVariable)));
 }
 
 void UnificationNode::print(const std::string &indent)
