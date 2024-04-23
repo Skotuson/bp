6a78d4d3e571d40af96bdb182db69e6192144387
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-04-23 11:53:44 +0200

    remove todo

--- src/wam_code/Instruction.cpp
+++ src/wam_code/Instruction.cpp
@@ -48,8 +48,6 @@ void Instruction::clearPDL(WAMState &state, std::shared_ptr<Word> X, std::shared
             state.trailPush(x);
             state.trailPush(y);
             // Bind them together
-            // TODO: bind both
-            // x->bind();
             y->bind(X);
         }
 
