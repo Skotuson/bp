6464b0a36a84189ecc1be512c7653af8909f9761
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-05-02 14:09:30 +0200

    create two mains

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,3 @@
-#define DOCTEST_CONFIG_IMPLEMENT
-#include "../test/doctest.h"
-
 #include <cstdio>
 #include <cassert>
 #include <iostream>
@@ -14,24 +11,6 @@
 
 int main(int argc, const char **argv)
 {
-
-    doctest::Context context;
-
-    // defaults
-    context.addFilter("test-case-exclude", "*math*"); // exclude test cases with "math" in their name
-    context.setOption("abort-after", 5);              // stop test execution after 5 failed assertions
-    context.setOption("order-by", "name");            // sort the test cases by their name
-
-    context.applyCommandLine(argc, argv);
-
-    // overrides
-    context.setOption("no-breaks", true); // don't break in the debugger when assertions fail
-
-    int res = context.run(); // run
-
-    if (context.shouldExit()) // important - query flags (and --exit) rely on the user doing this
-        return res;           // propagate the result of the tests
-
     Renderer renderer;
     Filepath sourceCodePath = "";
     int i = 0;
@@ -58,9 +37,9 @@ int main(int argc, const char **argv)
         std::cout << "Missing mandatory --file parameter" << std::endl;
         return 1;
     }
-
+    
     Preprocessor preprocessor;
-    Filepath fp = preprocessor.linkLibraries(sourceCodePath, "stdlib");
+    Filepath fp = preprocessor.linkLibraries(sourceCodePath, "../stdlib");
 
     std::ifstream ifs(fp);
 
@@ -74,5 +53,5 @@ int main(int argc, const char **argv)
 
     std::remove(fp.c_str());
 
-    return res;
+    return 0;
 }
---
+++ test/main.cpp
@@ -0,0 +1,21 @@
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "../test/doctest.h"
+
+int main(int argc, const char **argv)
+{
+    doctest::Context context;
+    // defaults
+    context.addFilter("test-case-exclude", "*math*"); // exclude test cases with "math" in their name
+    context.setOption("abort-after", 5);              // stop test execution after 5 failed assertions
+    context.setOption("order-by", "name");            // sort the test cases by their name
+
+    context.applyCommandLine(argc, argv);
+
+    // overrides
+    context.setOption("no-breaks", true); // don't break in the debugger when assertions fail
+
+    int res = context.run(); // run
+
+    if (context.shouldExit()) // important - query flags (and --exit) rely on the user doing this
+        return res;           // propagate the result of the tests
+}
