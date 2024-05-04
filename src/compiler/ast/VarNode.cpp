758a2f77c695ce625f4273e745440f630d6fa159
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-05-04 12:19:28 +0200

    add isWildcard attribute

--- src/compiler/ast/VarNode.cpp
+++ src/compiler/ast/VarNode.cpp
@@ -1,32 +1,27 @@
 #include "VarNode.hpp"
 
-VarNode::VarNode(const std::string &name)
-    : TermNode(name)
+VarNode::VarNode(const std::string &name, bool isWildcard)
+    : TermNode(name),
+      m_IsWildcard(isWildcard)
 {
 }
 
 void VarNode::codegen(CompilationContext &cctx)
 {
-    bool isWildcard = false;
-    std::string name = m_Name;
-    if (m_Name == "_")
-    {
-        name = cctx.generateTempVar();
-    }
-    cctx.noteVariable(name);
+    cctx.noteVariable(m_Name);
     if (!m_IsGoal)
     {
         cctx.addInstruction(
-            std::make_shared<GetVariableInstruction>(name, cctx.availableReg(), cctx.getVarOffset(name)));
+            std::make_shared<GetVariableInstruction>(m_Name, cctx.availableReg(), cctx.getVarOffset(m_Name)));
     }
     else
     {
-        if (!isWildcard)
+        if (!m_IsWildcard)
         {
-            cctx.addVariable(name);
+            cctx.addVariable(m_Name);
         }
         cctx.addInstruction(
-            std::make_shared<PutVariableInstruction>(name, cctx.availableReg(), cctx.getVarOffset(name)));
+            std::make_shared<PutVariableInstruction>(m_Name, cctx.availableReg(), cctx.getVarOffset(m_Name)));
     }
 
     cctx.setAvailableReg(cctx.availableReg() + 1);
