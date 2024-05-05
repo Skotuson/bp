1e624fa1b801f3b82966fd9d22e9c2ddf546fd99
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-05-06 01:42:58 +0200

    update structnode to call node

--- src/compiler/ast/UnificationNode.cpp
+++ src/compiler/ast/UnificationNode.cpp
@@ -1,5 +1,5 @@
 #include "UnificationNode.hpp"
-#include "StructNode.hpp"
+#include "CallNode.hpp"
 
 UnificationNode::UnificationNode(std::shared_ptr<TermNode> x, std::shared_ptr<TermNode> y)
     : m_X(x),
@@ -10,8 +10,8 @@ UnificationNode::UnificationNode(std::shared_ptr<TermNode> x, std::shared_ptr<Te
 void UnificationNode::codegen(CompilationContext &cctx)
 {
   std::vector<std::shared_ptr<TermNode>> args = {m_X, m_Y};
-  std::shared_ptr<StructNode> unifStruct = std::make_shared<StructNode>("__id", args);
-  unifStruct->codegen(cctx);
+  auto unifCall = std::make_shared<CallNode>("__id", args);
+  unifCall->codegen(cctx);
 }
 
 void UnificationNode::print(const std::string &indent)
