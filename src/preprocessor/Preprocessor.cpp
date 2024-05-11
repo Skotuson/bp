9ab4c416b90cb874f2ab923c74301db551c55037
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-05-11 18:57:13 +0200

    add and mul added to standard library

--- src/preprocessor/Preprocessor.cpp
+++ src/preprocessor/Preprocessor.cpp
@@ -9,7 +9,15 @@ Filepath Preprocessor::linkStandard(const Filepath &filepath)
 {
     std::istringstream iss(
         "__id(A, A).\n"
-        "__zero(0).");
+
+        "__add(0,Y,Y)."
+        "__add(__s(0),Y,__s(Y))."
+        "__add(__s(X),Y,__s(Z)):- add(X,Y,Z)."
+
+        "__mul(0,_,0)."
+        "__mul(__s(A),B,C) :-"
+        "__mul(A,B,D),"
+        "add(D,B,C).");
 
     return linkLibrary(filepath, iss);
 }
