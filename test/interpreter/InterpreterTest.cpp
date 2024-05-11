2ec2dc85ef5265b8e67682abc04d9ba0ab630060
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-05-12 00:58:58 +0200

    add is operator tests

--- test/interpreter/InterpreterTest.cpp
+++ test/interpreter/InterpreterTest.cpp
@@ -813,4 +813,90 @@ TEST_CASE("Interpreter test suite")
             testQuery(i, {true, {{"X", "foo(X)"}, {"Y", "foo(X)"}}});
         }
     }
+
+    SUBCASE("IS operator tests")
+    {
+        std::istringstream iss(
+            "__id(A, A).\n"
+
+            "__add(0,Y,Y)."
+            "__add(__s(0),Y,__s(Y))."
+            "__add(__s(X),Y,__s(Z)):- __add(X,Y,Z)."
+
+            // x - y = z is the same as y + z = x
+            "__sub(X, Y, Z) :- __add(Y, Z, X)."
+
+            "__mul(0,_,0)."
+            "__mul(__s(A),B,C) :-"
+            "__mul(A,B,D),"
+            "__add(D,B,C)."
+
+            // x / y = z is the same as y * z = x
+            "__div(X, Y, Z) :- __mul(Y, Z, X).");
+        c.compile(iss);
+
+        SUBCASE("Simple case")
+        {
+            Interpreter i(c.dump());
+            i.setQuery(i.compileQuery(
+                "1 is 1."));
+            testQuery(i, {true, {}});
+        }
+
+        SUBCASE("Simple failing case")
+        {
+            Interpreter i(c.dump());
+            i.setQuery(i.compileQuery(
+                "2 is 1."));
+            testQuery(i, {false, {}});
+        }
+
+        SUBCASE("Variable assignment I")
+        {
+            Interpreter i(c.dump());
+            i.setQuery(i.compileQuery(
+                "X is 1."));
+            testQuery(i, {true, {{"X", "1"}}});
+        }
+
+        SUBCASE("Variable assignment II")
+        {
+            Interpreter i(c.dump());
+            i.setQuery(i.compileQuery(
+                "X is (1 - 1) * 2."));
+            testQuery(i, {true, {{"X", "0"}}});
+        }
+
+        SUBCASE("Variable assignment III")
+        {
+            Interpreter i(c.dump());
+            i.setQuery(i.compileQuery(
+                "X is 2*3, Y is X * 2."));
+            testQuery(i, {true, {{"X", "6"}, {"Y", "12"}}});
+        }
+
+        SUBCASE("Variable assignment III")
+        {
+            Interpreter i(c.dump());
+            i.setQuery(i.compileQuery(
+                "X = 2, Y is X * 2."));
+            testQuery(i, {true, {{"X", "2"}, {"Y", "4"}}});
+        }
+
+        SUBCASE("Variable assignment III")
+        {
+            Interpreter i(c.dump());
+            i.setQuery(i.compileQuery(
+                "X = 2, Y is X * 2, Z is X + Y."));
+            testQuery(i, {true, {{"X", "2"}, {"Y", "4"}, {"Z", "6"}}});
+        }
+
+        SUBCASE("Variable assignment III")
+        {
+            Interpreter i(c.dump());
+            i.setQuery(i.compileQuery(
+                "X is 2-2/2."));
+            testQuery(i, {true, {{"X", "1"}}});
+        }
+    }
 }
