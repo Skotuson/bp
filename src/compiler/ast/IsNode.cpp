dde91d5abb87d13ea17dfd7e2a1fb07acd306d6f
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-05-11 19:14:23 +0200

    create codegen for IsNode

--- src/compiler/ast/IsNode.cpp
+++ src/compiler/ast/IsNode.cpp
@@ -1,5 +1,8 @@
 #include "IsNode.hpp"
 
+#include "UnificationNode.hpp"
+#include "VarNode.hpp"
+
 IsNode::IsNode(std::shared_ptr<TermNode> x, std::shared_ptr<TermNode> y)
     : m_X(x),
       m_Y(y)
@@ -8,6 +11,12 @@ IsNode::IsNode(std::shared_ptr<TermNode> x, std::shared_ptr<TermNode> y)
 
 void IsNode::codegen(CompilationContext &cctx)
 {
+    cctx.resetAvailableArithmeticVariable();
+    std::string varX, varY;
+    varX = m_X->codegen_arithmetic(cctx);
+    varY = m_Y->codegen_arithmetic(cctx);
+    auto unif = std::make_shared<UnificationNode>(std::make_shared<VarNode>(varX, true), std::make_shared<VarNode>(varY, true));
+    unif->codegen(cctx);
 }
 
 void IsNode::print(const std::string &indent)
