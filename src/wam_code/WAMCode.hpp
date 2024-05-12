77e33e700c3e499916087628dacc5f2f5fe4014f
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-05-12 19:44:03 +0200

    add comments

--- src/wam_code/WAMCode.hpp
+++ src/wam_code/WAMCode.hpp
@@ -21,26 +21,76 @@ struct WAMCode
 
     size_t size() const;
 
+    /**
+     * @brief Adds instructions to the code
+     * @param instructions Instructions to be added
+     */
     void addInstructions(const std::vector<std::shared_ptr<Instruction>> &instructions);
-    void deleteInstruction(size_t idx);
+
+    /**
+     * @brief Removes the last n instructions
+     * @param n Number of instructions to remove
+     */
     void popInstructions(size_t n);
 
+    /**
+     * @brief Iterates through the instructions and sets addresses of branch instructions
+     */
     void updateJumpInstructions(void);
 
+    /**
+     * @brief Merges current code with another code and updates jump instructions
+     */
     void merge(const WAMCode &code);
 
+    /**
+     * @param pc Index of the instruction to get
+     * @return Instruction at the index specified by the parametr
+     */
     std::shared_ptr<Instruction> getInstruction(size_t pc) const;
 
+    /**
+     * @brief Prints out an instruction. If the instruction is on the same address as label, prints the label aswell
+     * @param pc Instruction to print
+     * @param os Output stream to print the instruction to
+     */
     void dumpInstruction(size_t pc, std::ostream &os) const;
+
+    /**
+     * @brief Prints out all instructions
+     * @param os Output stream to print the instruction to
+     */
     void dump(std::ostream &os);
 
+    /**
+     * @brief Stores a variable
+     * @param v Variable to add
+     */
     void addVariable(const Variable &v);
+
+    /**
+     * @return Variable mapping in the form of offset -> variable name
+     */
     std::map<size_t, std::string> getVariables(void) const;
 
+    /**
+     * @brief Records a label in form of mapping address -> name of the label and vice versa
+     * @param label Label to add
+     */
     void addLabel(const Label &label);
+
+    /**
+     * @brief Deletes label
+     * @param label Label to delete
+     */
     void removeLabel(const Label &label);
+
+    /**
+     * @return Label address
+     */
     size_t getLabelAddress(const Label &label);
 
+private:
     std::map<size_t, Label> m_AddressToLabel;
     std::map<Label, size_t> m_LabelToAddress;
     std::vector<std::shared_ptr<Instruction>> m_Program;
