dee1cd1d37dddb6ca19c47f223a7cb156eb1de9d
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-05-12 16:52:51 +0200

    add comments to call node, add private:

--- src/compiler/ast/CallNode.cpp
+++ src/compiler/ast/CallNode.cpp
@@ -14,8 +14,10 @@ void CallNode::codegen(CompilationContext &cctx)
     {
         arg->codegen(cctx);
     }
+    // Use the functor and arity to create a callName, e.g. s/2 for s(x,y)
     std::string callName = m_Name + "/" + std::to_string(m_Args.size());
     cctx.addInstruction(std::make_shared<Call>(callName));
+    // Reset the avilable register back to 1
     cctx.setAvailableReg(1);
 }
 
--- src/compiler/ast/CallNode.hpp
+++ src/compiler/ast/CallNode.hpp
@@ -3,13 +3,18 @@
 #include "GoalNode.hpp"
 #include "TermNode.hpp"
 
+/**
+ * @brief Represents a node that will get translated to a call instruction in the bytecode.
+ * In the source code, this is a predicate in the body of a rule.
+ */
 struct CallNode : public GoalNode
 {
     CallNode(const std::string &name,
-               std::vector<std::shared_ptr<TermNode>> args = std::vector<std::shared_ptr<TermNode>>());
+             std::vector<std::shared_ptr<TermNode>> args = std::vector<std::shared_ptr<TermNode>>());
     void codegen(CompilationContext &cctx) override;
     void print(const std::string &indent = "") override;
 
+private:
     std::string m_Name;
     std::vector<std::shared_ptr<TermNode>> m_Args;
 };
