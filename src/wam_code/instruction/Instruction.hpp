631b324d2ea8546330ce68e1f4acbaf405df157c
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-05-12 20:00:09 +0200

    add comments

--- src/wam_code/instruction/Instruction.hpp
+++ src/wam_code/instruction/Instruction.hpp
@@ -9,12 +9,26 @@ struct Instruction
 {
     virtual std::shared_ptr<Instruction> clone(void) = 0;
     virtual ~Instruction(void) = default;
+    /**
+     * @brief Execute the instruction
+     * @param state Reference to WAMState class
+     */
     virtual void execute(WAMState &state) = 0;
     virtual void print(std::ostream &os) const = 0;
 
+    /**
+     * @brief Initiates the fail sequence
+     * @param state Reference to WAMState class
+     */
     static void fail(WAMState &state);
-    static void clearPDL(WAMState &state, std::shared_ptr<Word> X, std::shared_ptr<Word> Y);
 
+    /**
+     * @brief Executes the Clear PDL operation
+     * @param state Reference to WAMState class
+     * @param X Memory Word
+     * @param Y Memory Word
+     */
+    static void clearPDL(WAMState &state, std::shared_ptr<Word> X, std::shared_ptr<Word> Y);
     static const std::vector<std::vector<size_t>> m_ClearPDLTable;
 
     friend std::ostream &operator<<(std::ostream &os, const Instruction &instr);
