cfcc1a35b54a6a30df57b41e5ded6ed673552123
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-05-13 14:20:07 +0200

    Update debug.pl

--- examples/debug.pl
+++ examples/debug.pl
@@ -1,1 +1,19 @@
-decompose([H|T], H, T).
+a(Y) :- X = foo(X), X = Y. //foo(Y)
+b(X) :- X = Y, X = foo(X).
+c(X,Y) :- Y is X + 1.
+
+eval(+(A,B), R) :- R is A + B
+
+A is 2 + 2 //A = 4
+_a1 = A
+_a2 = 2
+_a3 = 2
+add(_a2,_a3, _a4)
+_a1 = a_4
+
+A is 2 + B, B = 2
+_a1 = A
+_a2 = 2
+_a3 = 2
+add(_a2,_a3, _a4)
+_a1 = a_4
