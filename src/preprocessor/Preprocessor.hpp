9f2ca45efc97920eae2e7c3dc77166bd76c1274f
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-05-14 14:23:45 +0200

    add preprocessor comments

--- src/preprocessor/Preprocessor.hpp
+++ src/preprocessor/Preprocessor.hpp
@@ -8,7 +8,19 @@ using Filepath = std::string;
 class Preprocessor
 {
 public:
+  /**
+   * @brief Links standard predicates to the source file
+   * @param filepath Path to the source file
+   * @return Returns the name of the file created by linking the source file and the standard library
+   */
   Filepath linkStandard(const Filepath &filepath);
+
+  /**
+   * @brief Links input stream contents and the source file
+   * @param filepath Path to the source file
+   * @param is Input stream who's content to link with the source file
+   * @return Returns the name of the file created by linking the source file and the contents of input stream is
+   */
   Filepath linkLibrary(const Filepath &filepath, std::istream &is);
 
 private:
