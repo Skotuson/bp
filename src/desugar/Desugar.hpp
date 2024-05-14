d6fc19e4010591ef139fbe1c29b2e317f38102e0
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-05-14 13:33:04 +0200

    Update Desugar.hpp

--- src/desugar/Desugar.hpp
+++ src/desugar/Desugar.hpp
@@ -12,5 +12,11 @@ public:
 
     static std::shared_ptr<TermNode> toPeanoNode(size_t num, bool underscores = false);
 
+    /**
+     * @brief Replaces all occurenses of Peano numbers represented as __s(__s(...__s(0)...)) with their natural number counterpart.
+     * Doesn't provide any check as the __s predicate is reserved.
+     * @param str String to replace peano numbers in
+     * @return String with all peano numbers replaced with their natural numbers counterpart
+     */
     static std::string replacePeano(const std::string &str);
 };
