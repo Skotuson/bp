3745ab377b907bfe225c8ae504337a23ac4da3e4
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-05-14 14:04:44 +0200

    add allocate comments

--- src/wam_code/instruction/procedural/Allocate.cpp
+++ src/wam_code/instruction/procedural/Allocate.cpp
@@ -20,12 +20,14 @@ void Allocate::execute(WAMState &state)
         // In case the variable is a query variable and the environment is the first choice point, add the name.
         if (state.isQueryVariable(i) && state.SReg() == 1)
         {
+            // Make the unbound variable word point to its memory location
             auto vw = std::make_shared<VariableWord>(&cp->m_Variables[i], state.getQueryVariableName(i));
             cp->m_Variables[i] = vw;
             state.addQueryWord(i, vw);
         }
         else
         {
+            // In case its not a query variable, replace the name with a pattern __<number> to avoid name collisions
             cp->m_Variables[i] = std::make_shared<VariableWord>(&cp->m_Variables[i], "__" + std::to_string(state.getAllocatedVariables()));
             state.setAllocatedVariables(state.getAllocatedVariables() + 1);
         }
