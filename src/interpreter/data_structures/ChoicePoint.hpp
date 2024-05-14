e0b226c414c334a90afb0119a9eb36c15308d82b
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-05-14 15:07:42 +0200

    add comments to ChoicePoint

--- src/interpreter/data_structures/ChoicePoint.hpp
+++ src/interpreter/data_structures/ChoicePoint.hpp
@@ -7,15 +7,22 @@
 struct ChoicePoint
 {
     ChoicePoint(const ArgumentRegisters &argReg, size_t bce, size_t bcp, size_t bb, size_t btr, size_t bh, size_t fa);
-
+    
     friend std::ostream &operator<<(std::ostream &os, const ChoicePoint &cp);
-
+    // Copy of the argument registers
     ArgumentRegisters m_ArgumentRegisters;
-    size_t m_BCE; // Backtrack Continuation Environment
-    size_t m_BCP; // Backtrack Continuation Pointer
-    size_t m_BB;  // Backtrack B (register)
-    size_t m_BTR; // Backtrack Trail Top
-    size_t m_BH; // Backtrack Heap Top
-    size_t m_FA;  // Failure Address
+    // Backtrack Continuation Environment
+    size_t m_BCE;
+    // Backtrack Continuation Pointer
+    size_t m_BCP;
+    // Backtrack B (register)
+    size_t m_BB;
+    // Backtrack Trail Top
+    size_t m_BTR;
+    // Backtrack Heap Top
+    size_t m_BH;
+    // Failure Address
+    size_t m_FA;
+    // Variables for the local environment
     std::vector<std::shared_ptr<Word>> m_Variables;
 };
