e1a4eb53e5fe442e71ab1c4ed7b4fbed49c2f383
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-05-14 13:45:08 +0200

    add cctx doc comments

--- src/compiler/CompilationContext.hpp
+++ src/compiler/CompilationContext.hpp
@@ -53,20 +53,69 @@ public:
      */
     size_t allocated(void);
 
+    /**
+     * @brief Adds a variable to the WAMCode instance, used for user-entered variables
+     * @param variable Name of the variable to be added
+     */
     void addVariable(const std::string &variable);
+
+    /**
+     * @brief Notes a variable, used in the compilation process, used in bytecode generation.
+     * @param variable Name of the variable to be added
+     */
     void noteVariable(const std::string &variable);
+
+    /***
+     * @brief Returns the offset of a variable in th current environment
+     * @param variable Name of a variable
+     * @return Variable's offset in the current environment
+     */
     size_t getVarOffset(const std::string &variable);
+
+    /**
+     * @brief Resets all variables stored in the context, used in the beginning of a clause-level compilation.
+     */
     void resetVariables(void);
 
+    /**
+     * @return Returns next available register to be used in bytecode generation.
+     */
     size_t availableReg(void);
+
+    /**
+     * @brief Sets the next available register to a value specified by the parameter
+     * @param reg Register to be set as next available
+     */
     void setAvailableReg(size_t reg);
 
+    /**
+     * @return Returns the name of the next available arithmetic variable, used in is operator bytecode generation.
+     */
     std::string getAvailableArithmeticVariable(void);
+
+    /**
+     * @brief Resets the next avilable arithmetic variable to the default value 0
+     */
+
+    /**
+     * @brief Increases the next avilable arithmetic variable by one
+     */
     void resetAvailableArithmeticVariable(void);
     void incrementAvailableArithmeticVariable(void);
 
+    /**
+     * @return The current mode that the clause's compilation process is currently in
+     */
     CodeGenerationMode mode(void);
+
+    /**
+     * @brief Set the compilation mode to HEAD
+     */
     void setHeadGenerationMode(void);
+
+    /**
+     * @brief Set the compilation mode to BODY
+     */
     void setBodyGenerationMode(void);
 
 private:
@@ -77,6 +126,5 @@ private:
 
     // Used in Clause-Level compilation
     std::map<std::string, size_t> m_Variables;
-
     CodeGenerationMode m_CGMode = HEAD;
 };
