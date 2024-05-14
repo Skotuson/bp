cfa6a7c0f1a1d58fad91aba6391d87af220235e2
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2024-05-14 15:25:07 +0200

    add argReg comments

--- src/interpreter/data_structures/ArgumentRegisters.hpp
+++ src/interpreter/data_structures/ArgumentRegisters.hpp
@@ -12,7 +12,17 @@ public:
     ArgumentRegisters(const ArgumentRegisters &argReg);
     ArgumentRegisters &operator=(const ArgumentRegisters &argReg);
 
+    /**
+     * @brief Load a specific register
+     * @param word Word to load into a register
+     * @param reg Number of register to load. As the registers are labeled from 1, this method accounts for it
+     */
     void fillRegister(std::shared_ptr<Word> word, size_t reg);
+
+    /**
+     * @brief Dereferences a specific register. If a reference is encountered, it is chased until something other than a reference is encountered.
+     * @param reg Number of register to dereference. As the registers are labeled from 1, this method accounts for it
+     */
     std::shared_ptr<Word> dereferenceRegister(size_t reg) const;
 
     friend std::ostream &operator<<(std::ostream &os, const ArgumentRegisters &argReg);
